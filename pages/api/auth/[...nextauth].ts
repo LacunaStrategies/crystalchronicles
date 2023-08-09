add03fc2ad7958c67ae7e44cc2f1f6107a562f95
Dustin Hunter <102769956+LacunaStrategies@users.noreply.github.com>
2023-08-08 23:00:46 -0700

    Update [...nextauth].ts
    Added MongoDB Adapter; Updated NextAuthOptions import

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,23 @@
-import NextAuth from "next-auth"
-import type { NextAuthOptions } from "next-auth"
+// ** NextAuth Imports
+import NextAuth, { NextAuthOptions } from "next-auth"
 import Github from "next-auth/providers/github"
 
+// ** MongoDB Imports
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "@/lib/mongodb/client"
+
 export const authOptions: NextAuthOptions = {
-  providers: [
-    Github({
-        clientId: process.env.GITHUB_ID || '',
-        clientSecret: process.env.GITHUB_SECRET || '',
-    }),
-  ],
+    providers: [
+        Github({
+            clientId: process.env.GITHUB_ID || '',
+            clientSecret: process.env.GITHUB_SECRET || '',
+        }),
+    ],
+    // @ts-expect-error
+    adapter: MongoDBAdapter(clientPromise),
+    session: {
+        strategy: 'jwt',
+    },
 }
 
 export default NextAuth(authOptions)
